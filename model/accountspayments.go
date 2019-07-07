462c856282335418341b6c8bc4f2bac69091e52d
chattip.s <chattip.soontaku@ifyoucan.com>
2019-07-07 10:35:22 +0700

    Added omitempty

--- model/accountspayments.go
+++ model/accountspayments.go
@@ -3,16 +3,21 @@ package model
 type AccountsPaymentsItem struct {
 	Embedded struct {
 		Records []struct {
-			ID          string `json:"id"`
-			PagingToken string `json:"paging_token"`
-			Type        string `json:"type"`
-			CreatedAt   string `json:"created_at"`
-			AssetType   string `json:"asset_type"`
-			AssetCode   string `json:"asset_code"`
-			AssetIssuer string `json:"asset_issuer"`
-			From        string `json:"from"`
-			To          string `json:"to"`
-			Amount      string `json:"amount"`
+			ID                    string `json:"id"`
+			PagingToken           string `json:"paging_token"`
+			TransactionSuccessful bool   `json:"transaction_successful"`
+			Type                  string `json:"type"`
+			CreatedAt             string `json:"created_at"`
+			TransactionHash       string `json:"transaction_hash"`
+			StartingBalance       string `json:"starting_balance,omitempty"`
+			Funder                string `json:"funder,omitempty"`
+			Account               string `json:"account,omitempty"`
+			AssetType             string `json:"asset_type,omitempty"`
+			AssetCode             string `json:"asset_code,omitempty"`
+			AssetIssuer           string `json:"asset_issuer,omitempty"`
+			From                  string `json:"from,omitempty,omitempty"`
+			To                    string `json:"to,omitempty"`
+			Amount                string `json:"amount,omitempty"`
 		} `json:"records"`
 	} `json:"_embedded"`
 }
